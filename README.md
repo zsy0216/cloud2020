cc6f9a836b22cd56bcff4a00c7264aa98eb000f8
zsy0216 <594983498@qq.com>
2020-08-02 13:17:34 +0800

    feat: 构建 Eureka 集群-eureka-server7002

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 - [Bilibili-尚硅谷2020最新版SpringCloud(H版&alibaba)框架开发教程全套完整版从入门到精通(大牛讲授spring cloud)](https://www.bilibili.com/video/BV18E411x7eT/)
 
 - [我的其他文档](https://zsy0216.github.io/docs/)
+
+#### Eureka 集群修改 host 文件
+找到C:\Windows\System32\drivers\etc路径下的hosts文件,添加
+```shell script
+127.0.0.1 eureka7001.com
+127.0.0.1 eureka7002.com
+```
---
+++ cloud-eureka-server7002/src/main/java/org/example/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author shuaiyin.zhang
+ * @description
+ * @date 2020/08/02
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7002 {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaMain7002.class, args);
+	}
+}
