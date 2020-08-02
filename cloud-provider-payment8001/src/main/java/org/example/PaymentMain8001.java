f6df8df88514790b353b2d93b34a15203f7a713a
zsy0216 <594983498@qq.com>
2020-08-02 12:31:07 +0800

    feat: 将 cloud-provider-payment8001 注册进 Eureka Server 成为服务提供者 provider

--- cloud-provider-payment8001/src/main/java/org/example/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/org/example/PaymentMain8001.java
@@ -2,12 +2,14 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shuaiyin.zhang
  * @description payment8001 主启动类
  * @date 2020/08/01
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentMain8001 {
 	public static void main(String[] args) {
