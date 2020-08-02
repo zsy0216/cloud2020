f31136e524efe595206b95a6e5978a0be2ceccb2
zsy0216 <594983498@qq.com>
2020-08-02 12:42:07 +0800

    feat: 将 cloud-consumer-order80 注册进 Eureka Server 成为 Eureka Client 服务消费者 consumer

--- cloud-consumer-order80/src/main/java/org/example/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/org/example/OrderMain80.java
@@ -2,12 +2,14 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shuaiyin.zhang
  * @description
  * @date 2020/08/01
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
 	public static void main(String[] args) {
