553b3db12347ee601545d94716fe594a1db5db7c
zsy0216 <594983498@qq.com>
2020-08-04 14:59:27 +0800

    docs: RestTemplate 方法说明 增加用例

--- cloud-consumer-order80/src/main/java/org/example/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/org/example/controller/OrderController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.common.CommonResult;
 import org.example.entity.Payment;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,30 @@ public class OrderController {
 	@GetMapping("/payment/get/{id}")
 	public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
 		return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+	}
+
+	/**
+	 * RestTemplate 方法介绍
+	 * <p>
+	 * 1. getForObject(): 返回对象为响应体中数据转化的对象，基本上可以理解为json
+	 * 2. getForEntity(): 返回对象为 ResponseEntity 对象，包含了相应中的一些重要信息，比如响应头、响应状态码、响应体等
+	 * 3. postForObject():
+	 * 4. postForEntity():
+	 */
+
+	@GetMapping("/payment/getForEntity/{id}")
+	public CommonResult<Payment> getPaymentForEntity(@PathVariable("id") Long id) {
+		ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+		if (entity.getStatusCode().is2xxSuccessful()) {
+			log.info(entity.getStatusCode() + "\t" + entity.getHeaders());
+			return entity.getBody();
+		} else {
+			return new CommonResult<>(444, "请求失败");
+		}
+	}
+
+	@GetMapping("/payment/createForEntity")
+	public CommonResult createForEntity(Payment payment) {
+		return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class).getBody();
 	}
 }
