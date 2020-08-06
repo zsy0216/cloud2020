6b12cbb8884dea238bd464fe1cc0a5ac67821556
zsy0216 <594983498@qq.com>
2020-08-06 09:17:46 +0800

    fix: 修改响应数据信息

--- cloud-provider-payment8002/src/main/java/org/example/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/org/example/controller/PaymentController.java
@@ -41,7 +41,7 @@ public class PaymentController {
 		Payment payment = paymentService.getPaymentById(id);
 		log.info("查询结果: {}", payment);
 		if (payment != null) {
-			return new CommonResult(200, "插入数据库成功, 服务端口: " + serverPort, payment);
+			return new CommonResult(200, "查询数据成功, 服务端口: " + serverPort, payment);
 		} else {
 			return new CommonResult(444, "没有对应记录, 查询 id: " + id);
 		}
