5e21db14d392f9c2207a00e94cb4042c1172f1c7
zsy0216 <594983498@qq.com>
2020-08-10 14:38:52 +0800

    feat: 通过JavaConfig的方式配置路由访问百度新闻

---
+++ cloud-gateway-gateway9527/src/main/java/org/example/config/GatewayConfig.java
@@ -0,0 +1,34 @@
+package org.example.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author shuaiyin.zhang
+ * @description
+ * @date 2020/08/10
+ */
+@Configuration
+public class GatewayConfig {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+		routes.route("path_rote_tassel",
+				r -> r.path("/guonei")
+				.uri("http://news.baidu.com/guonei"))
+				.build();
+		routes.route("path_rote_tassel2",
+				r -> r.path("/guoji")
+				.uri("http://news.baidu.com/guoji"))
+				.build();
+		routes.route("path_rote_tassel3",
+				r -> r.path("/**")
+				.uri("http://news.baidu.com/**"))
+				.build();
+		return routes.build();
+	}
+
+}
