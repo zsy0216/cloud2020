d65b011f7852b49ad45a07f82573d5d360404106
zsy0216 <594983498@qq.com>
2020-08-10 16:47:39 +0800

    feat: 构建 Config3344 配置总控中心

---
+++ cloud-config-center-3344/src/main/java/org/example/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author shuaiyin.zhang
+ * @description
+ * @date 2020/08/10
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenterMain3344.class, args);
+	}
+}
