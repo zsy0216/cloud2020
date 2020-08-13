77228b6a9368112ffe85f370312dcb7ae3266110
zsy0216 <594983498@qq.com>
2020-08-13 11:38:31 +0800

    test: test 热点 key 限流

--- cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package org.example.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -24,7 +27,6 @@ public class FlowLimitController {
 		return "------testB";
 	}
 
-
 	@GetMapping("/testD")
 	public String testD() {
 		try {
@@ -34,6 +36,21 @@ public class FlowLimitController {
 		}
 		log.info("testD 测试RT");
 		return "------testD";
+	}
+
+	@GetMapping("/testHotKey")
+	@SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+	public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+							 @RequestParam(value = "p2", required = false) String p2) {
+		//int age = 10/0;
+		return "------testHotKey";
+	}
+
+	/**
+	 * 兜底方法
+	 */
+	public String deal_testHotKey(String p1, String p2, BlockException exception) {
+		return "------deal_testHotKey,o(╥﹏╥)o";
 	}
 }
 
