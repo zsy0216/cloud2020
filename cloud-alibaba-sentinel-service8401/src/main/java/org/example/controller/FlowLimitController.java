fca664cd8a8794c713a9003deffe3ea3a2f5a63c
zsy0216 <594983498@qq.com>
2020-08-13 11:13:39 +0800

    test: test 降级策略 RT

--- cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/FlowLimitController.java
@@ -1,15 +1,18 @@
 package org.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author shuaiyin.zhang
  * @description
  * @date 2020/08/13
  */
-
 @RestController
+@Slf4j
 public class FlowLimitController {
 	@GetMapping("/testA")
 	public String testA() {
@@ -19,6 +22,18 @@ public class FlowLimitController {
 	@GetMapping("/testB")
 	public String testB() {
 		return "------testB";
+	}
+
+
+	@GetMapping("/testD")
+	public String testD() {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		log.info("testD 测试RT");
+		return "------testD";
 	}
 }
 
