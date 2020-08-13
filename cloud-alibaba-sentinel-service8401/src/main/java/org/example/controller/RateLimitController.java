5d0a1148e82e1ba38e0bac51dd25377b464af86a
zsy0216 <594983498@qq.com>
2020-08-13 14:06:45 +0800

    test: test @SentinelResource 按URL地址限流

--- cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/RateLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/org/example/controller/RateLimitController.java
@@ -16,11 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class RateLimitController {
 	@GetMapping("/byResource")
 	@SentinelResource(value = "byResource", blockHandler = "handleException")
-	public CommonResult byResource() {
-		return new CommonResult(200, "按资源名称限流测试OK", new Payment(2020L, "serial001"));
+	public CommonResult<Payment> byResource() {
+		return new CommonResult<>(200, "按资源名称限流测试OK", new Payment(2020L, "serial001"));
 	}
 
-	public CommonResult handleException(BlockException exception) {
-		return new CommonResult(444, exception.getClass().getCanonicalName() + ": 服务不可用");
+	public CommonResult<Payment> handleException(BlockException exception) {
+		return new CommonResult<>(444, exception.getClass().getCanonicalName() + ": 服务不可用");
+	}
+
+	@GetMapping("/rateLimit/byUrl")
+	@SentinelResource(value = "byUrl")
+	public CommonResult<Payment> byUrl() {
+		return new CommonResult<>(200, "按url限流测试OK", new Payment(2020L, "serial002"));
 	}
 }
