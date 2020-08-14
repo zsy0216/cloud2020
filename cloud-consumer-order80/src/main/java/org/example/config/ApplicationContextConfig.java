64483858e750f1d917d67637d127ea4f0c3ea8e4
zsy0216 <594983498@qq.com>
2020-08-14 13:46:13 +0800

    test: test eureka 7001 & 7002 cluster

--- cloud-consumer-order80/src/main/java/org/example/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/org/example/config/ApplicationContextConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
 
-	//@LoadBalanced  //注意：注释掉这个注解后，之前通过服务名称远程调用会失效，不注释则无法通过URI进行调用(自写的轮询负载均衡算法)
+	@LoadBalanced  //注意：注释掉这个注解后，之前通过服务名称远程调用会失效，不注释则无法通过URI进行调用(自写的轮询负载均衡算法)
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
