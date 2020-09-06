87208c0d051ce1d216b298f6ca06701df51fbf5b
zsy0216 <594983498@qq.com>
2020-09-06 09:24:57 +0800

    test: show plan

--- cloud-consumer-order80/src/main/java/org/example/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/org/example/OrderMain80.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
-//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMain80.class, args);
